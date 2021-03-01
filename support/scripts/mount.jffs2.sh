f74cd527d79486d38fdfee890c056d5cadcdb535
Alex Markessinis <markea125@gmail.com>
2021-03-01 12:42:35 -0500

    working on enabling usb eth0 on boot

--- support/scripts/mount.jffs2.sh
+++ support/scripts/mount.jffs2.sh
@@ -1,48 +1,75 @@
-#!/bin/bash
+#!/bin/sh
+#
+# script to mount a jffs2-image a loopback device
+# ©Thomas Fischer 2008
+# This script may be distributed, altered, reverse-engineered,
+# rebranded or thrown across the room at leisure, as long as
+# nobody blames me for it.
+#
+# Chapter one: Sanity Test
+#   Find out if what we're doing is actually making any sense
+#   Note: this section could use a good expanding. Meanwhile,
+#   it's the ole 'Think before you type'
+#
+# Oh, and of course we need to react to the cry for help:
+case "$1" in
 
-## Script to mount jffs2 filesystem using mtd kernel modules.
-## EMAC, Inc. 2009
+--help|-h)
+	 echo Usage: mntjffs \<jffs2-image\> \<mountpoint\>
+	 echo
+	 echo Script will exit with a return code of 0 in case of success,
+	 echo return code of 1 indicates UID not 0, return code of 2
+	 echo indicates sanity check failed, return code of 3 finally indicates
+	 echo something went wrong during the mount procedure
+	 ;;
 
-if [[ $# -lt 2 ]]
-then
-    echo "Usage: $0 FSNAME.JFFS2 MOUNTPOINT [ERASEBLOCK_SIZE]"
-    exit 1
+# Except for that, we let things take their natural course...
+*)
+
+SANITY_CHECK=0
+if [ $(file -b $1 |cut -d ' ' -f2) = "jffs2" ] && [ -d $2 ]; then
+
+  SANITY_CHECK=1
+  echo Sanity check passed...
+
 fi
 
-if [ "$(whoami)" != "root" ]
-then
-    echo "$0 must be run as root!"
-    exit 1
+
+# Chapter Two: Mounting
+#   probe all the mods, set up the loops, mount the mtds.
+#   check first if we are root, if we are not, the probing
+#   might be a wee difficult
+#
+#   FYI: The sleeps are necessary because for some reason
+#   the /dev/mtdblock devices take a second or two to show up.
+if [ `id -u` -ne 0 ]; then
+  echo "This script needs to be run as root"
+  exit 1
 fi
 
-if [[ ! -e $1 ]]
-then
-    echo "$1 does not exist"
-    exit 1
-fi
-
-if [[ ! -d $2 ]]
-then
-    echo "$2 is not a valid mount point"
-    exit 1
-fi
-
-if [[ "$3" == "" ]]
-then
-	esize="128"
+if [ $SANITY_CHECK -eq 1 ]; then
+	export loop=$(losetup -f) && \
+	losetup $loop $1 && \
+	modprobe block2mtd block2mtd=$loop,131072 && \
+	sleep 2 && \
+	modprobe jffs2 && \
+	sleep 2 && \
+	modprobe mtdblock && \
+	sleep 2 && \
+	mount -t jffs2 -o rw /dev/mtdblock0 $2 && \
+	( echo -n "Image ";echo -n $1;echo -n " sucessfully mounted on ";echo $2 ) && \
+	exit 0 || echo mount failed, check dmesg \|tail for pointers;exit 3
 else
-	esize="$3"
+	echo SANITY CHECK FAILED!
+	echo
+	echo Something\'s not right here, chummer.
+	echo Make sure you have a valid jffs2 image AND a valid directory to mount it on.
+	echo And remember: Think before you type, or bad things may happen -- all sanity
+	echo checking of this world will not save your butt if you act foolishly
+	echo
+	echo  Usage: mntjffs \<jffs2-image\> \<mountpoint\>
+	exit 2
 fi
+;;
 
-# cleanup if necessary
-umount /dev/mtdblock0 &>/dev/null
-modprobe -r mtdram &>/dev/null
-modprobe -r mtdblock &>/dev/null
-
-modprobe mtdram total_size=32768 erase_size=$esize || exit 1
-modprobe mtdblock || exit 1
-dd if="$1" of=/dev/mtdblock0 || exit 1
-mount -t jffs2 /dev/mtdblock0 $2 || exit 1
-
-echo "Successfully mounted $1 on $2"
-exit 0
+esac
