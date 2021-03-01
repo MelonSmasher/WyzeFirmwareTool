1440235cbcaefc43b988133621cc0d197151e2bf
Alex Markessinis <markea125@gmail.com>
2021-03-01 17:39:43 -0500

    working on enabling usb eth0 on boot

--- fwtool/mods/__init__.py
+++ fwtool/mods/__init__.py
@@ -14,13 +14,13 @@ def enable_usbnet(squashfs_1, squashfs_2, jffs2):
     shutil.copyfile('support/mods/ko/usbnet.ko', os.path.join(squashfs_2, 'usbnet.ko'))
     shutil.copyfile('support/mods/ko/asix.ko', os.path.join(squashfs_2, 'asix.ko'))
     print('Done!')
-    print('Copying custom script', end='... ')
-    eth_file = os.path.join(jffs2, 'bin', 'eth0_init.sh')
-    shutil.copyfile('support/mods/scripts/eth0_init.sh', eth_file)
-    subprocess.run(['sudo', 'chmod', '+x', eth_file])
-    subprocess.run(['sudo', 'chown', '501:0', eth_file])
-    print('Done!')
-    print('Updating init file', end='... ')
+    #print('Copying custom script', end='... ')
+    #eth_file = os.path.join(jffs2, 'bin', 'eth0_init.sh')
+    #shutil.copyfile('support/mods/scripts/eth0_init.sh', eth_file)
+    #subprocess.run(['sudo', 'chmod', '+x', eth_file])
+    #subprocess.run(['sudo', 'chown', '501:0', eth_file])
+    #print('Done!')
+    print('Updating init files', end='... ')
     init_files = [
         os.path.join(jffs2, 'init', 'app_init.sh'),
         os.path.join(jffs2, 'init', 'app_init_xiao.sh'),
@@ -29,7 +29,24 @@ def enable_usbnet(squashfs_1, squashfs_2, jffs2):
     for init_file in init_files:
         with open(init_file, 'r+') as f:
             text = f.read()
-            text = re.sub('/system/bin/sinker &', '/system/bin/sinker &\n/system/bin/eth0_init.sh &', text)
+            #text = re.sub('/system/bin/sinker &', '/system/bin/sinker &\n/system/bin/eth0_init.sh &', text)
+            text = re.sub(
+                '/system/bin/sinker &',
+                '/system/bin/sinker &\n\nifconfig eth0 up\nudhcpc -i eth0 -p /var/run/udhcpc_eth0.pid -b',
+                text
+            )
+            if os.path.join(jffs2, 'init', 'app_init_da.sh') == init_file:
+                text = re.sub(
+                    'insmod /driver/audio.ko',
+                    'insmod /driver/audio.ko\ninsmod /driver/usbnet.ko\ninsmod /driver/asix.ko',
+                    text
+                )
+            else:
+                text = re.sub(
+                    'insmod /driver/rtl8189ftv.ko',
+                    'insmod /driver/rtl8189ftv.ko\ninsmod /driver/usbnet.ko\ninsmod /driver/asix.ko',
+                    text
+                )
             f.seek(0)
             f.write(text)
             f.truncate()
