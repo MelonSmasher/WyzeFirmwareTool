216d4454ec989eb5d17a10aae80123617d05c585
Alex Markessinis <markea125@gmail.com>
2021-03-02 11:34:32 -0500

    attempting to clean up leftover dhcp process from wlan and bring down the wlan interface

--- support/mods/scripts/eth0_init.sh
+++ support/mods/scripts/eth0_init.sh
@@ -6,6 +6,6 @@ USB_COUNT=$(lsusb | wc -l)
 
 if [ $USB_COUNT -ge 2 ]; then
   ifconfig eth0 up
-  sleep 9
+  sleep 1
   udhcpc -i eth0 -p /var/run/udhcpc_eth0.pid -b
 fi
--- support/mods/scripts/wlan0_kill.sh
+++ support/mods/scripts/wlan0_kill.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 
-killall -9 udhcpc
-killall -9 udhcpc
-sleep 5
-killall -9 udhcpc
-killall -9 udhcpc
-killall -9 wpa_supplicant
-killall -9 udhcpc
-killall -9 udhcpc
-sleep 5
+# Take a hammer to wpa_supplicant
 killall -9 wpa_supplicant
 sleep 5
 killall -9 wpa_supplicant
-sleep 5
-killall -9 wpa_supplicant
+
+# Take down wlan0
 ifconfig wlan0 down
+
+# Kill off udhcpc -t 10 -i wlan0 -p /var/run/udhcpc.pid -b
+while :
+do
+  udhcpc_pid=$(ps | grep udhcpc | grep wlan0 | awk '{$1=$1};1' | cut -d ' ' -f 1)
+  if [ -z "${udhcpc_pid}" ];
+    exit 0
+  else
+    kill -9 $udhcpc_pid
+  fi
+  sleep 1
+done
