57ee134a2d26413c221a7e5b8e97d2c618e1e5cc
Alex Markessinis <markea125@gmail.com>
2021-03-02 12:21:08 -0500

    attempting to clean up leftover dhcp process from wlan and bring down the wlan interface

--- support/mods/scripts/wlan0_kill.sh
+++ support/mods/scripts/wlan0_kill.sh
@@ -1,32 +1,41 @@
 #!/bin/sh
 
-# Wait for everything to start
-sleep 5
+kill_wpa_supplicant() {
+  # Take a hammer to wpa_supplicant
+  look_for_wpa_supplicant=true
+  while $look_for_wpa_supplicant
+  do
+    wpa_supplicant_pid=$(ps | grep wpa_supplicant | grep wlan0 | awk '{$1=$1};1' | cut -d ' ' -f 1)
+    if [ -z "${wpa_supplicant_pid}" ]; then
+      look_for_wpa_supplicant=false
+    else
+      killall -9 wpa_supplicant
+    fi
+    sleep 30
+  done
+}
 
+kill_udhcpc() {
+  look_for_udhcpc=true
+  while $look_for_udhcpc
+  do
+    udhcpc_pid=$(ps | grep udhcpc | grep wlan0 | awk '{$1=$1};1' | cut -d ' ' -f 1)
+    if [ -z "${udhcpc_pid}" ]; then
+      exit 0
+    else
+      kill -9 $udhcpc_pid
+    fi
+    sleep 30
+  done
+}
+
+# Wait for everything to start
+sleep 10
 # Take down wlan0
 ifconfig wlan0 down
-
 # Take a hammer to wpa_supplicant
-look_for_wpa_supplicant=true
-while $look_for_wpa_supplicant
-do
-  wpa_supplicant_pid=$(ps | grep wpa_supplicant | grep wlan0 | awk '{$1=$1};1' | cut -d ' ' -f 1)
-  if [ -z "${wpa_supplicant_pid}" ]; then
-    look_for_wpa_supplicant=false
-  else
-    killall -9 wpa_supplicant
-  fi
-  sleep 15
-done
-
-# Kill off udhcpc -t 10 -i wlan0 -p /var/run/udhcpc.pid -b
-while :
-do
-  udhcpc_pid=$(ps | grep udhcpc | grep wlan0 | awk '{$1=$1};1' | cut -d ' ' -f 1)
-  if [ -z "${udhcpc_pid}" ]; then
-    exit 0
-  else
-    kill -9 $udhcpc_pid
-  fi
-  sleep 15
-done
+kill_wpa_supplicant &
+# Kill off udhcpc for wlan0
+kill_udhcpc &
+# Wait for background functions to complete
+wait
