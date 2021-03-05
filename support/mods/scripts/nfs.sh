f4a79d07ce5d2e16f3d41287475c43d7ea1faf92
Alex Markessinis <markea125@gmail.com>
2021-03-05 13:05:13 -0500

    updated nfs mount script

--- support/mods/scripts/nfs.sh
+++ support/mods/scripts/nfs.sh
@@ -9,6 +9,7 @@ mount_nfs() {
 
   local NFS_MOUNT="/bin/mount $NFS_OPTIONS"
   local RETRY_COUNT=0
+
   while true; do
     let RETRY_COUNT=$RETRY_COUNT+1
     if [ $RETRY_COUNT -gt 100 ]; then
@@ -16,12 +17,16 @@ mount_nfs() {
     fi
 
     if ! /bin/mount | grep -q "$NFS_ROOT on $NFS_MOUNT_DIR"; then
+      # Make our mount dir
+      mkdir -p $NFS_MOUNT_DIR
+      # Mount the NFS volume
       if ! $NFS_MOUNT $NFS_ROOT $NFS_MOUNT_DIR; then
         sleep 10
         continue
       fi
     fi
 
+    # Determine the NFS share directory for this camera based on the device ID/MAC
     local CAM_DIR=$NFS_MOUNT_DIR/WyzeCams/$DEVICE_ID
     for DIR in $NFS_MOUNT_DIR/WyzeCams/*/; do
       if [ -f "$DIR/.mac_$DEVICE_ID" ]; then
@@ -30,6 +35,7 @@ mount_nfs() {
       fi
     done
 
+    # Make the camera's directory if it does not exists
     if [ ! -d "$CAM_DIR" ]; then
       echo "WyzeHack: Creating data directory [$CAM_DIR]"
       if ! mkdir -p "$CAM_DIR"; then
@@ -39,8 +45,7 @@ mount_nfs() {
       fi
     fi
 
-    rm -rf "${SD_ROOT}/*" && rm -rf "${SD_ROOT}/.*"
-
+    # Bind mount over the SD card
     if ! mount -o bind "${CAM_DIR}" "${SD_ROOT}"; then
       echo "Bind $CAM_DIR as ${SD_ROOT} failed, will retry..."
       sleep 5
@@ -57,9 +62,11 @@ MY_NFS_HOST=
 MY_NFS_ROOT=
 MY_NFS_OPTS=
 
+# Wait for the camera to have network to reach the NFS server
 printf "%s" "waiting for NFS Server ..."
 while ! ping -c 1 -n $MY_NFS_HOST &>/dev/null; do
   printf "%c" "."
 done
 
+# Run the mount function
 mount_nfs "${MY_NFS_ROOT}" "${MY_NFS_OPTS}"
