b4cf23d70a5a125c498bda69d533377a74da65c6
Alex Markessinis <markea125@gmail.com>
2021-03-04 21:04:49 -0500

    updated wlan0_kill.sh to begin killing wlan related processed once eth0 has an IP

--- support/mods/scripts/wlan0_kill.sh
+++ support/mods/scripts/wlan0_kill.sh
@@ -10,7 +10,7 @@ kill_wpa_supplicant() {
     else
       killall -9 wpa_supplicant
     fi
-    sleep 30
+    sleep 5
   done
 }
 
@@ -18,23 +18,33 @@ kill_udhcpc() {
   look_for_udhcpc=true
   while $look_for_udhcpc
   do
-    sleep 30
     udhcpc_pid=$(ps | grep udhcpc | grep -v "grep" | grep wlan0 | awk '{$1=$1};1' | cut -d ' ' -f 1)
     if [ -z "${udhcpc_pid}" ]; then
       look_for_udhcpc=false
     else
       kill -9 $udhcpc_pid
     fi
+    sleep 5
   done
 }
 
-# Wait for everything to start
-sleep 25
-# Take down wlan0
-ifconfig wlan0 down
-# Take a hammer to wpa_supplicant
-kill_wpa_supplicant &
-# Kill off udhcpc for wlan0
-kill_udhcpc &
-# Wait for background functions to complete
-wait
+# Infinite loop to ensure that eth0 ends up with an IP
+while :
+do
+  # Get the IP for eth0
+  eth0_ip=$(ifconfig eth0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}')
+  # Check if there is an IP
+  if [ -z "${eth0_ip}" ]; then
+    # If there is not an IP lets wait
+    sleep 1
+  else
+    # Take down wlan0
+    ifconfig wlan0 down
+    # Take a hammer to wpa_supplicant
+    kill_wpa_supplicant &
+    # Kill off udhcpc for wlan0
+    kill_udhcpc &
+    # Wait for background functions to complete
+    wait
+  fi
+done
