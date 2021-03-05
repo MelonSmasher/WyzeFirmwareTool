bd4fd081a2a32930cdbf9b234102160b2738ad01
Alex Markessinis <markea125@gmail.com>
2021-03-04 19:27:37 -0500

    testing a new entry point that. This aviods any modifications to jffs2/init.app_init.sh

--- support/mods/scripts/eth0_init.sh
+++ support/mods/scripts/eth0_init.sh
@@ -5,8 +5,6 @@ sleep 5
 USB_COUNT=$(lsusb | wc -l)
 
 if [ $USB_COUNT -ge 2 ]; then
-  insmod /driver/usbnet.ko
-  insmod /driver/asix.ko
   ifconfig eth0 up
   udhcpc -i eth0 -p /var/run/udhcpc_eth0.pid -b
 fi
--- support/mods/scripts/mod_hooks.sh
+++ support/mods/scripts/mod_hooks.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 
-look_for_icamera=true
-while $look_for_icamera
-do
-  pid=$(ps | grep iCamera | grep -v "grep" | awk '{$1=$1};1' | cut -d ' ' -f 1)
-  if [ -z "${pid}" ]; then
-    sleep 1
-  else
-    look_for_icamera=false
-  fi
-done
+load_eth0_kos() {
+  insmod /driver/usbnet.ko
+  insmod /driver/asix.ko
+}
 
+look_for_icamera() {
+  look_for_icamera=true
+  while $look_for_icamera; do
+    pid=$(ps | grep iCamera | grep -v "grep" | awk '{$1=$1};1' | cut -d ' ' -f 1)
+    if [ -z "${pid}" ]; then
+      sleep 1
+    else
+      look_for_icamera=false
+    fi
+  done
+}
+
+# load_eth0_kos
+look_for_icamera
 # /root/mods/eth0_init.sh &
 # /root/mods/wlan0_kill.sh &
