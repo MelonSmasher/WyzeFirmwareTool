fef9e99deb1b936cf1d3b9803a8441147e37ef04
Alex Markessinis <markea125@gmail.com>
2021-03-04 19:51:51 -0500

    testing a new entry point that. This aviods any modifications to jffs2/init.app_init.sh

--- support/mods/scripts/eth0_init.sh
+++ support/mods/scripts/eth0_init.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-sleep 8
+sleep 5
 if [ $(lsusb | wc -l) -ge 2 ]; then
-  ifconfig eth0 up && udhcpc -i eth0 -p /var/run/udhcpc_eth0.pid -b
+  sleep 1
+  ifconfig eth0 up
+  sleep 1
+  udhcpc -i eth0 -p /var/run/udhcpc_eth0.pid -b
 fi
